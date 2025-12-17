d27506a3d437c039ef2248c0299c46c813910d30
nikolai <sportislife635@gmail.com>
2025-12-17 16:24:42 +0300

    add CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,11 +3,17 @@ from app.api.routes import cards
 from app.api.routes import cards, groups
 import app.models
 from app.api.routes import auth
-
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Flashcards API")
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # 👈 фронт
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cards.router, prefix="/cards", tags=["cards"])
 app.include_router(groups.router, prefix="/groups", tags=["groups"])
